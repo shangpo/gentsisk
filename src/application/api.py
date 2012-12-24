e8f12fa7dd9caef7e9e2ba4bf5507b428044f9db
Adam Ashley <aashley@adamashley.name>
2012-12-24 13:25:56 +0800

    Correct API auth mode, and add more debug to cache

--- src/application/api.py
+++ src/application/api.py
@@ -1,15 +1,14 @@
 from eveapi import eveapi
 import settings
 import blobcache
+import logging
 
 
 def get_api():
+    api = eveapi.EVEAPIConnection(cacheHandler=apiMemcacheHandler())
     if len(settings.API_KEYID) > 0 and len(settings.API_VCODE):
-        api = eveapi.EVEAPIConnection(keyID=settings.API_KEY_ID,
-                vCode=settings.API_VCODE,
-                cacheHandler=apiMemcacheHandler())
-    else:
-        api = eveapi.EVEAPIConnection(cacheHandler=apiMemcacheHandler())
+        api.auth(keyID=settings.API_KEYID,
+                vCode=settings.API_VCODE)
     return api
 
 class apiMemcacheHandler(object):
@@ -20,8 +19,7 @@ class apiMemcacheHandler(object):
 
     def log(self, what):
         if self.debug:
-            return
-#print "[%d] %s" % (self.count, what)
+            logging.info("[%d] %s", self.count, what)
 
     def retrieve(self, host, path, params):
         key = "api-%d" % hash((host, path, frozenset(params.items())))
@@ -33,12 +31,13 @@ class apiMemcacheHandler(object):
             self.log("%s in cache" % path)
             return cached
 
-        self.log("%s not cached % path")
+        self.log("%s not cached" % path)
         return None
 
     def store(self, host, path, params, doc, obj):
         key = "api-%d" % hash((host, path, frozenset(params.items())))
 
         cachedFor = obj.cachedUntil - obj.currentTime
+        self.log("%s added to the cache for %d" % (path, cachedFor))
         blobcache.set(key, doc, namespace="eveapi", time=cachedFor)
 
