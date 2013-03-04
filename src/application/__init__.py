00640912977ff674b05f36d0bb6e38761eadc8e8
Adam Ashley <aashley@adamashley.name>
2013-03-04 09:07:21 +0800

    Add Jinja2 filter for formatting ISK values correctly

--- src/application/__init__.py
+++ src/application/__init__.py
@@ -5,10 +5,18 @@ Initialize Flask app
 
 from flask import Flask
 from flaskext.gae_mini_profiler import GAEMiniProfiler
+from inspect import getmembers, isfunction
+import jinja_filters
 
 
 app = Flask('application')
 app.config.from_object('application.settings')
+
+my_filters = {name: function
+                for name, function in getmembers(jinja_filters)
+                if isfunction(function)}
+
+app.jinja_env.filters.update(my_filters)
 
 # Enable profiler (enabled in non-production environ only)
 GAEMiniProfiler(app)
---
+++ src/application/jinja_filters.py
@@ -0,0 +1,3 @@
+
+def format_isk(value):
+    return "{:,.2f} ISK".format(value)
